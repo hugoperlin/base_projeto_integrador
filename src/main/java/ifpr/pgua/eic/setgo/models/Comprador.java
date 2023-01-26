49ba3a111ea7b264e99a29e0e30a118c128f56e9
b055 <you@example.com>
2023-01-26 11:04:05 -0300

    criação das classes de usuarios

---
+++ src/main/java/ifpr/pgua/eic/setgo/models/Comprador.java
@@ -0,0 +1,10 @@
+package ifpr.pgua.eic.setgo.models;
+
+public class Comprador extends Usuario{
+
+    public Comprador(int cpf, String nome, String email, String telefone) {
+        super(cpf, nome, email, telefone);
+        //TODO Auto-generated constructor stub
+    }
+    
+}
---
+++ src/main/java/ifpr/pgua/eic/setgo/models/Produtor.java
@@ -0,0 +1,10 @@
+package ifpr.pgua.eic.setgo.models;
+
+public class Produtor extends Usuario{
+
+    public Produtor(int cpf, String nome, String email, String telefone) {
+        super(cpf, nome, email, telefone);
+        //TODO Auto-generated constructor stub
+    }
+    
+}
